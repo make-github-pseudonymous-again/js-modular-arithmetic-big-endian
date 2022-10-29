07f9c0dc3cc0c0a34f6b6d7d186fb643e3662bcf
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2022-10-29 13:56:23 +0200

    :books: docs: Add leading '@' to project name.

--- doc/scripts/header.js
+++ doc/scripts/header.js
@@ -10,7 +10,7 @@ const domReady = function (callback) {
 domReady(() => {
 	const projectname = document.createElement('a');
 	projectname.classList.add('project-name');
-	projectname.text = 'arithmetic-operations-for/integers-modulo-n-big-endian';
+	projectname.text = '@arithmetic-operations-for/integers-modulo-n-big-endian';
 	projectname.href = './index.html';
 
 	const header = document.querySelector('header');
